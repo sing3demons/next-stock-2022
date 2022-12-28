bfcdf090a6c83cb19212f7a217054a97c1ef16f1
sing3demons <sing3demons@live.com>
2022-12-28 23:08:47 +0700

    :sparkles: call action redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Layout from '@/components/Layout/Layout'
 import { useSelector } from 'react-redux'
-import { userSelector } from '../store/slices/userSlice';
+import { userSelector, setUsername } from '@/store/slices/userSlice'
+import { useAppDispatch } from '@/store/hook'
 
 type Props = {}
 
 const Index = (props: Props) => {
   // const userSelector = useSelector((store: any) => store.user)
   const { username } = useSelector(userSelector)
+  const dispatch = useAppDispatch()
 
   return (
     <Layout>
       <div>name : {username}</div>
+      <button onClick={() => dispatch(setUsername({ username: 'sing' }))}>Click</button>
     </Layout>
   )
 }
--- store/slices/userSlice.ts
+++ store/slices/userSlice.ts
@@ -1,5 +1,5 @@
 import { User } from '@/models/user.model'
-import { createSlice } from '@reduxjs/toolkit'
+import { ActionReducerMapBuilder, PayloadAction, ValidateSliceCaseReducers, createSlice } from '@reduxjs/toolkit'
 import { RootState } from '@/store/store'
 
 interface UserState {
@@ -11,18 +11,39 @@ interface UserState {
   user?: User
 }
 
-const name = 'user'
+interface SingleProp {
+  username: string
+}
+
+const name: 'user' = 'user'
+
 const initialState: UserState = {
-  username: 'sing',
+  username: 'aaa',
   accessToken: '',
   isAuthenticated: false,
   isAuthenticating: true,
   user: undefined,
 }
-const reducers = {}
+// const reducers = {
+//   setUsername: (state: UserState, action: PayloadAction<any>) => {
+//     state.username = action.payload.data
+//   },
+// }
+const extraReducers = (builder: ActionReducerMapBuilder<UserState>) => {}
 
 // export const userSelector = (store: RootState) => store.user
 export const userSelector = ({ user }: RootState) => user
 
-const userSlice = createSlice({ name, initialState, reducers })
+const userSlice = createSlice({
+  name,
+  initialState,
+  reducers: {
+    setUsername: (state: UserState, action: PayloadAction<SingleProp>) => {
+      state.username = action.payload.username
+    },
+  },
+  extraReducers,
+})
+
+export const { setUsername } = userSlice.actions
 export default userSlice.reducer
